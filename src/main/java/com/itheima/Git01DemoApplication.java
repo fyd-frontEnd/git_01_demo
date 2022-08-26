463c74634d20b9c8c0125e7b046bb0b318afe0b2
fengyudong <fffyd931115@163.com>
2022-08-26 14:55:00 +0800

    first commit

---
+++ src/main/java/com/itheima/Git01DemoApplication.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fyd
+ * @version 1.0.0
+ * 2022/8/26 14:46
+ */
+@SpringBootApplication
+public class Git01DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git01DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/itheima/controller/WelcomController.java
@@ -0,0 +1,21 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fyd
+ * @version 1.0.0
+ * 2022/8/26 14:46
+ */
+@RestController
+public class WelcomController {
+
+
+    @GetMapping()
+    public Object index(){
+        return "hello spring boot ";
+    }
+
+}
